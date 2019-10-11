3bc92d4ca367fe8ebbd635078eaf1c728df71b4d
雷传涛 <leichuantao@fcbox.com>
2019-10-11 10:53:02 +0800

    [gitignore] change gitignore

---
+++ app/src/main/java/com/example/chtlei/mydemo/utils/BitmapBuilder.java
@@ -0,0 +1,37 @@
+package com.example.chtlei.mydemo.utils;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+
+public class BitmapBuilder {
+
+    public static void calculateOptionsById(@NonNull Resources res,@NonNull BitmapFactory.Options options, int imgId) {
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, imgId, options);
+    }
+
+    public static int calculateInSamplesizeByOptions(@NonNull BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        int inSampleSize   = 1;
+        int originalWidth  = options.outWidth;
+        int originalHeight = options.outHeight;
+        if (originalHeight > reqHeight || originalWidth > reqWidth) {
+            int heightRatio = originalHeight / reqHeight;
+            int widthRatio  = originalWidth  / reqWidth;
+            inSampleSize = heightRatio > widthRatio ? heightRatio : widthRatio;
+        }
+        options.inJustDecodeBounds = false;
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeBitmapById (@NonNull Resources res, int resId, int reqWidth, int reqHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        calculateOptionsById(res, options, resId);
+        options.inSampleSize = calculateInSamplesizeByOptions(options, reqWidth, reqHeight);
+        options.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeResource(res, resId, options);
+        return bitmap;
+    }
+}
